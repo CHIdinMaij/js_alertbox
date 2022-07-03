c430cf829acfb91d50803f9b1621ae1a81979113
chidinma <cijeoma214@gmail.com>
2022-07-03 23:51:14 +0100

    Add files via upload
    It is an alert box

---
+++ alert.js
@@ -0,0 +1,10 @@
+
+var result = confirm("Do you really want to leave this page?");
+if (result == true) {
+    alert("Thanks for visiting");
+}
+else {
+    alert("Thanks for staying with us");
+}        
+		var user = prompt("Please enter your name");
+alert(user+ "you are welcome");
